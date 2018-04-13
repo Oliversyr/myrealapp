5847f036b0705e9c71bf9ce81b8416d04ac9dc91
Oliversyr <yang368r@163.com>
2018-04-13 11:16:26 +0800

    first real app

---
+++ myserve/serveApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ServeappConfig(AppConfig):
+    name = 'serveApp'
---
+++ myserve/serveApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+#coding:utf-8
+
+# from django.http import HttpResponse
+from django.shortcuts import HttpResponse
+# import json
+# import requests
+
+def index(request):
+    return HttpResponse('133')
